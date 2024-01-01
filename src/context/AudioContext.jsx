ae59a7040eb70a96fd9985d05e5f5bca77dc7915
redennessy14 <136608186+redennessy14@users.noreply.github.com>
2024-01-01 14:25:34 +0600

    first commit

---
+++ src/context/AudioContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState } from "react";
+
+import tracksList from "../assets/tracksList";
+
+export const AudioContext = createContext({});
+
+const defaultTrack = tracksList[0];
+
+const audio = new Audio(defaultTrack.src);
+
+const AudioProvider = ({ children }) => {
+  const [currentTrack, setCurrentTrack] = useState(defaultTrack);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleToggleAudio = (track) => {
+    if (currentTrack.id !== track.id) {
+      setCurrentTrack(track);
+      setIsPlaying(true);
+      audio.src = track.src;
+      audio.currentTime = 0;
+      audio.play();
+
+      return;
+    }
+    if (isPlaying) {
+      audio.pause();
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const value = { audio, currentTrack, isPlaying, handleToggleAudio };
+
+  return (
+    <AudioContext.Provider value={value}>{children}</AudioContext.Provider>
+  );
+};
+
+export default AudioProvider;
